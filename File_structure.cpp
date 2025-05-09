04b4e6ce08a012c2bf04b20fe2df056cf9bdb716
Cyber Er. Rahul jha <142599405+SilentProfessor@users.noreply.github.com>
2025-05-09 10:29:51 +0530

    Create File_structure.cpp

---
+++ File_structure.cpp
@@ -0,0 +1,11 @@
+bug_tracker/
+├── app.py
+├── templates/
+│   ├── index.html
+│   ├── add_bug.html
+│   ├── edit_bug.html
+├── static/
+│   ├── style.css
+├── bug_tracker.db
+├── requirements.txt
+└── README.md
